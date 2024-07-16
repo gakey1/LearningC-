0aff26a0c2eca98bc0c1aa6684c6e4d8ad073099
Gakey1 <gakeyapple@gmail.com>
2024-07-17 09:33:43 +1000

    Initial commit

---
+++ TemperatureConverter/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*
+ * ===================================
+ *          FUNCTIONS C++
+ * ===================================
+ * Functions have got 3 main parts:
+ * Inputs: Data you give to the function to work with, called parameters.
+ * Processes: Code inside the function that does something with the input.
+ * Outputs: This is what the functions gives back after it finishes processing; return value
+ * Importance of functions;
+ * Re-usability: Write code and use it many times
+ * Organisation: Break down complex problems into smaller, manageable pieces.
+ * Maintenance: Easier to update and manage the code.
+ * */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
